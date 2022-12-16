dc3a227c9061b74ff7b729f9317150fc0cc163a2
haocq300303 <chuhao878@gmail.com>
2022-12-16 17:27:19 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import { connectDB } from "./src/config/db.js";
-import router from "./src/routes/index.js";
-
+// import router from "./src/routes/index.js";
+import routerAuth from "./src/routes/auth.js";
+import routerComment from "./src/routes/commentRoute.js";
+import routerPost from "./src/routes/postRoute.js";
+import routerUser from "./src/routes/userRoute.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -18,7 +21,10 @@ app.use(morgan("combined"));
 app.use(express.json());
 
 // routes
-router(app);
+app.use("/api/auth", routerAuth);
+app.use("/api/users", routerUser);
+app.use("/api/posts", routerPost);
+app.use("/api/comments", routerComment);
 
 // handle Error
 app.use((err, req, res, next) => {
